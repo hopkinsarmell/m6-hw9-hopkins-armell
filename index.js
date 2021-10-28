06ddfe347d3014c8104aa07bd40b3c016b1d3b1d
hopkinsarmell <hopkinsarmell@ufl.edu>
2021-10-28 10:37:36 -0400

    add code to index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector('button')
+var forecastEl = document.getElementById('forecast')
+
+
+
+btn.onclick = function() {
+    //console.log('clicked')
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=00f5bbd24e7c673c1cf820cd3c75f578")
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(res) {
+        console.log(res)
+    })
+}
+
