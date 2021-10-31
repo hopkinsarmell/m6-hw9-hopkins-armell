2f85eacd0f75461f4412849ab6c1cd45ad309247
hopkinsarmell <hopkinsarmell@ufl.edu>
2021-10-31 13:15:13 -0400

    add code to return city name to page

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ var forecastEl = document.getElementById('forecast')
 formEl.onsubmit = function(e) {
     e.preventDefault()
     var query = inputEl.value
-    console.log(query)
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid=00f5bbd24e7c673c1cf820cd3c75f578')
+    //console.log(query)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+query+'&units=imperial&appid=00f5bbd24e7c673c1cf820cd3c75f578')
     .then(function(res) {
         return res.json()
     })
-    .then(function(res) {
-    console.log(res)
+    .then(function(result) {
+    renderWeather(result)
     })
+}
+
+function renderWeather(weatherObj) {
+    //console.log(weatherObj.name)
+    //refreshes name upon new search
+    forecastEl.innerHTML = ""
+    //add city name to page by creating an element
+    var name = document.createElement('h2')
+    name.textContent = weatherObj.name + ""
+    forecastEl.appendChild(name)
 }
 
