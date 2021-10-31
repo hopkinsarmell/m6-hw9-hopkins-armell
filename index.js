b823d4f6d4a64b9046274a84631daa2bd4883430
hopkinsarmell <hopkinsarmell@ufl.edu>
2021-10-31 11:16:34 -0400

    add submit click event and return in json

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ var formEl = document.querySelector('form')
 var inputEl = document.querySelector('input')
 var forecastEl = document.getElementById('forecast')
 
-
-
-btn.onclick = function() {
-    //console.log('clicked')
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=00f5bbd24e7c673c1cf820cd3c75f578")
+//add an event to form submit
+//insert user input into fetch link
+//return data in json format
+formEl.onsubmit = function(e) {
+    e.preventDefault()
+    var query = inputEl.value
+    console.log(query)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid=00f5bbd24e7c673c1cf820cd3c75f578')
     .then(function(res) {
         return res.json()
     })
     .then(function(res) {
-        console.log(res)
+    console.log(res)
     })
 }
 
