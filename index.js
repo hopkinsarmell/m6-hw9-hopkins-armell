51357565d5ea9944a5c06809cff7137958b08245
hopkinsarmell <hopkinsarmell@ufl.edu>
2021-10-31 13:40:23 -0400

    add temp and feels like temp to index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ formEl.onsubmit = function(e) {
     e.preventDefault()
     var query = inputEl.value
     //console.log(query)
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+query+'&units=imperial&appid=00f5bbd24e7c673c1cf820cd3c75f578')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+query+'&units=imperial&sys.country&appid=00f5bbd24e7c673c1cf820cd3c75f578')
     .then(function(res) {
         return res.json()
     })
@@ -25,7 +25,19 @@ function renderWeather(weatherObj) {
     forecastEl.innerHTML = ""
     //add city name to page by creating an element
     var name = document.createElement('h2')
-    name.textContent = weatherObj.name + ""
+    var country = document.createElement('h2')
+    name.textContent = weatherObj.name + "" + ", " + weatherObj.sys.country + ""
     forecastEl.appendChild(name)
+    forecastEl.appendChild(country)
+
+    
+    //temperature
+    var temp = document.createElement('p')
+    temp.textContent = "Current: " + weatherObj.main.temp + "℉"
+    forecastEl.appendChild(temp)
+
+    var feels_like = document.createElement('p')
+    feels_like.textContent = "Feels like: " + weatherObj.main.feels_like + "℉"
+    forecastEl.appendChild(feels_like)
 }
 
